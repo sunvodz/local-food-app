f6fef738e18a65b1cc6fe9f57d874c7470598318
David Ajnered <david@ajnered.com>
2018-06-04 20:44:27 +0200

    Removed unused config package

--- app/shared/sharedActions.js
+++ app/shared/sharedActions.js
@@ -1,7 +1,6 @@
 import { AsyncStorage } from 'react-native';
 import { Permissions, Notifications, Location } from 'expo';
 import _ from 'lodash';
-import config from '../../config';
 import api from './api';
 import * as sharedActionTypes from './sharedActionTypes';
 
--- config/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  scopes: [
-    'users-read-self',
-    'users-orders-read',
-    'users-orders-modify',
-    'users-nodes-read',
-    'users-nodes-modify',
-  ]
-}
